cd80b5bc682deec36dac75b95ea3fe5ac1e1ae7b
Stéfani <stehlavigne@gmail.com>
2024-05-31 19:12:34 +0100

    function to sort by button

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ async function getVideos() {
                     <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `
@@ -72,3 +73,23 @@ function filtrarPesquisa() {
 //     }
 // }
 
+const botaoCategoria = document.querySelectorAll(".superior__item");
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro) {   
+    const videos = document.querySelectorAll(".videos__item");
+
+    for (let video of videos) {
+        let categoriaVideo = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+        if (!categoriaVideo.includes(valorFiltro) && valorFiltro != "tudo") {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
